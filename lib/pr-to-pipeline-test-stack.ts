98670404c98b4134c53498e353143532658330c3
Rayjan Wilson <rayjwil@amazon.com>
2021-05-27 11:31:06 -0400

    base files

--- lib/pr-to-pipeline-test-stack.ts
+++ lib/pr-to-pipeline-test-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
-
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigatewayv2');
+import integrations = require('@aws-cdk/aws-apigatewayv2-integrations');
 export class PrToPipelineTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
