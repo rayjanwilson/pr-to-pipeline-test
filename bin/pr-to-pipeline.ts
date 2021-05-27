30646744d93e36da1612ecd8bf23fbf555a3cb0c
Rayjan Wilson <rayjwil@amazon.com>
2021-05-27 18:03:38 -0400

    looks like we need the file for persisting temp info

--- bin/pr-to-pipeline.ts
+++ bin/pr-to-pipeline.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import { existsSync, readFileSync } from 'fs';
 import { PipelineStack } from '../cicd-pipeline/pipeline';
 
 const github = {
@@ -12,6 +13,9 @@ const github = {
 if (process.env.CODEBUILD_WEBHOOK_HEAD_REF) {
   console.log(`i can see the branch is ${process.env.CODEBUILD_WEBHOOK_HEAD_REF}`);
   github.branch = process.env.CODEBUILD_WEBHOOK_HEAD_REF.split('/').pop() || 'master';
+} else if (existsSync('pr_branch.txt')) {
+  const data = readFileSync('pr_branch.txt', 'utf-8');
+  github.branch = data.split('/').pop() || 'master';
 }
 
 const app = new cdk.App();
