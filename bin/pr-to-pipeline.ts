90555fdc4b099409e6e533d080a157abf6adf499
Rayjan Wilson <rayjwil@amazon.com>
2021-05-27 17:34:46 -0400

    should build a pipeline for this branch

--- bin/pr-to-pipeline.ts
+++ bin/pr-to-pipeline.ts
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { PrToPipelineStack } from '../lib/pr-to-pipeline-stack';
+import { PipelineStack } from '../cicd-pipeline/pipeline';
+
+const github = {
+  owner: 'rayjanwilson',
+  repo: 'pr-to-pipeline-test',
+  branch: 'master',
+};
+
+if (process.env.CODEBUILD_WEBHOOK_HEAD_REF) {
+  console.log(`i can see the branch is ${process.env.CODEBUILD_WEBHOOK_HEAD_REF}`);
+  github.branch = process.env.CODEBUILD_WEBHOOK_HEAD_REF.split('/').pop() || 'master';
+}
 
 const app = new cdk.App();
-new PrToPipelineStack(app, 'PrToPipelineStack');
+new PipelineStack(app, `PR2P-${github.branch}`, { github });
--- test/pr-to-pipeline-test.test.ts
+++ test/pr-to-pipeline-test.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as PrToPipeline from '../lib/pr-to-pipeline-stack';
+import { GenericAppStack } from '../lib/generic-app';
 
 test('Empty Stack', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new PrToPipeline.PrToPipelineStack(app, 'MyTestStack');
+  const stack = new GenericAppStack(app, 'MyTestStack');
   // THEN
   expectCDK(stack).to(
     matchTemplate(
