e8dc964876e240753a869005fd503d70362a83ee
Rayjan Wilson <rayjwil@amazon.com>
2021-06-12 12:52:39 -0400

    draft of the extensive imagebuilder for docker

--- cicd-pipeline/imagebuilder-docker.ts
+++ cicd-pipeline/imagebuilder-docker.ts
@@ -1,11 +1,24 @@
-import { Construct } from '@aws-cdk/core';
-import { CfnContainerRecipe, CfnComponent } from '@aws-cdk/aws-imagebuilder';
+import { Construct, Stack } from '@aws-cdk/core';
+import {
+  CfnContainerRecipe,
+  CfnComponent,
+  CfnInfrastructureConfiguration,
+  CfnDistributionConfiguration,
+  CfnImage,
+} from '@aws-cdk/aws-imagebuilder';
 import { Repository } from '@aws-cdk/aws-ecr';
+import { Vpc } from '@aws-cdk/aws-ec2';
+import { CfnInstanceProfile, Role, ServicePrincipal, ManagedPolicy } from '@aws-cdk/aws-iam';
 
 export class ImageBuilderDocker extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-    const temp_ecr = new Repository(this, 'TempRepo');
+
+    const { account, region } = Stack.of(scope);
+
+    const temp_ecr = new Repository(this, 'TempRepo', {
+      imageScanOnPush: true,
+    });
 
     const generic_component = new CfnComponent(this, 'GenericComponent', {
       name: 'generic-container-image-component',
@@ -13,13 +26,57 @@ export class ImageBuilderDocker extends Construct {
       version: '1.0.0',
     });
 
-    const Recipe = new CfnContainerRecipe(this, 'ExImage', {
+    const recipe = new CfnContainerRecipe(this, 'ExImage', {
       name: 'AmazonLinux2-Container-Recipe',
       version: '1.0.0',
       parentImage: 'amazonlinux:latest',
       containerType: 'DOCKER',
       components: [{ componentArn: generic_component.attrArn }],
       targetRepository: { repositoryName: temp_ecr.repositoryName },
+    });
+
+    const role = new Role(this, 'instancerole', {
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+        ManagedPolicy.fromAwsManagedPolicyName('EC2InstanceProfileForImageBuilderECRContainerBuilds'),
+      ],
+      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+      path: '/executionServiceEC2Role/',
+    });
+    const instanceprofile = new CfnInstanceProfile(this, 'instance_profile', {
+      path: '/executionServiceEC2Role/',
+      roles: [role.roleArn],
+    });
+    const build_vpc = new Vpc(this, 'vpc');
+    const infrastructure = new CfnInfrastructureConfiguration(this, 'Infra', {
+      name: 'imagebuilder_infra',
+      instanceProfileName: instanceprofile.ref,
+      instanceTypes: ['t4.micro'],
+      terminateInstanceOnFailure: false,
+    });
+
+    const distribution = new CfnDistributionConfiguration(this, 'distrib', {
+      name: 'Generic-Container-DistributionConfiguration',
+      description:
+        'This distribution configuration will deploy our container image to the desired target ECR repository in the current region',
+      distributions: [
+        {
+          region: region,
+          containerDistributionConfiguration: {
+            Repository: temp_ecr.repositoryName,
+          },
+        },
+      ],
+    });
+
+    const IBImage = new CfnImage(this, 'IBImage', {
+      imageRecipeArn: recipe.attrArn,
+      infrastructureConfigurationArn: infrastructure.attrArn,
+      distributionConfigurationArn: distribution.attrArn,
+      imageTestsConfiguration: {
+        imageTestsEnabled: true,
+        timeoutMinutes: 20,
+      },
     });
   }
 }
